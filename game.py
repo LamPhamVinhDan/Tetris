43c90840ffe796e69c40d401a8d2996a7cfd02f2
Bearr123 <21520677@gm.uit.edu.vn>
2024-05-01 22:59:44 +0700

    New

--- game.py
+++ game.py
@@ -17,13 +17,14 @@ class Game:
 		pygame.mixer.music.load("Sounds/music.ogg")
 		pygame.mixer.music.play(-1)
 
-	def update_score(self, lines_cleared):
+	def update_score(self, lines_cleared, move_down_points):
 		if lines_cleared == 1:
 			self.score += 100
 		elif lines_cleared == 2:
 			self.score += 300
 		elif lines_cleared == 3:
 			self.score += 500
+		self.score += move_down_points
 
 	def get_random_block(self):
 		if len(self.blocks) == 0:
